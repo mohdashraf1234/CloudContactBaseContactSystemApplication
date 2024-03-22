b762568dcf1f17191ac30ec658258632a3d10c66
Mohd Ashraf <98030979+mohdashraf1234@users.noreply.github.com>
2024-03-22 06:16:42 +0530

    Add files via upload

---
+++ CloudBasedContactApplication/src/main/java/com/smart/CloudBasedContactApplication.java
@@ -0,0 +1,13 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBasedContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBasedContactApplication.class, args);
+	}
+
+}
